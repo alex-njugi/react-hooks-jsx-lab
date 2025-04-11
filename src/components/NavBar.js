863aee7036156830cd93cb4ab2afc64ccaa27c01
alex-njugi <alexnjugi11@gmail.com>
2025-04-11 14:52:04 +0300

    yay

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,19 @@
 import React from "react";
 
 function NavBar() {
-  // update the JSX being returned!
-  return <nav>NavBar</nav>;
+
+  return(
+    <div>
+      <nav>
+        <ul>
+          <li><a href="#home">Home</a>
+          </li>
+          <li><a href="#about">About</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  )
 }
 
 export default NavBar;
